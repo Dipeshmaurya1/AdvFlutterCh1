db2f9f42863b6133a0742aea4f0c898a428dd3dc
dipesh <dipeshmaurya152@gmail.com>
2024-07-14 20:54:21 +0530

    14-07-24

--- lib/Screens/DailyTask/Task 1.4/Provider/theme_change_provider.dart
+++ lib/Screens/DailyTask/Task 1.4/Provider/theme_change_provider.dart
@@ -2,13 +2,8 @@ import 'package:flutter/material.dart';
 
 class ChangeThemeProvider extends ChangeNotifier {
   bool isDark = false;
- bool darkMode = false;
   void changeTheme(bool value) {
     isDark = value;
-    notifyListeners();
-  }
-  void changeName(bool value) {
-    darkMode = value;
     notifyListeners();
   }
 }
